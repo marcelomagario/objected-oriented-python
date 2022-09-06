10e435cb997784b34f782737db9b1e8b18acae11
Marcelo Magario <marcelomagario@gmail.com>
2022-09-06 10:16:28 +1000

    Finalização do algoritmo

--- contagem-caracteres/contagem_caracteres_lista.py
+++ contagem-caracteres/contagem_caracteres_lista.py
@@ -1,9 +1,30 @@
-# Algoritmo para avaliar uma string de entrada.
-# Contar a quantidade de cada caracteres da string.
-# Mostrar em ordem alfabética
-'''
-Ex.
->>> s = 'Marcelo'
+"""
+- Algoritmo para avaliar uma string de entrada.
+- Contar a quantidade de cada caracteres da string.
+- Mostrar em ordem alfabética
+    Ex.
+    >>> contar_caracteres('marcelo')
+    a = 1
+    c = 1
+    e = 1
+    l = 1
+    m = 1
+    o = 1
+    r = 1
+    """
+
+def contar_caracteres(s):
+    """Função que conta caracteres de uma string"""
+    caracteres_ordenados = sorted(s) # cria uma lista ordenada
+    caracter_anterior = caracteres_ordenados[0]
+    contador = 1
+    for i in caracteres_ordenados[1:]:
+        if i == caracter_anterior:
+            contador += 1
+        else:
+            print(f'{caracter_anterior} = {contador}')
+            caracter_anterior = i
+            contador = 1
+    print(f'{caracter_anterior} = {contador}')
 
 
-'''
