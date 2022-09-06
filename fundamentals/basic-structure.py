72500242a97d58cab9f9c696cefa0ba59bf6fc0f
Marcelo Magario <marcelomagario@gmail.com>
2022-09-07 08:58:56 +1000

    Create a Constructor and it's attributes -OOP

--- fundamentals/basic-structure.py
+++ fundamentals/basic-structure.py
@@ -1,6 +1,13 @@
 # Class
 # Syntax
 
-# Brand, Memory, Video
+# Brand, Memory RAM, Video Board
 
+# Constructor. This method called when an object is created from the class
+#  it allow the class to initialize the attributes of a class.
+class Computer():
+    def __init__(self, brand, memory_ram, video_board):
+        self.video_board = video_board
+        self.memory_ram = memory_ram
+        self.brand = brand
 
