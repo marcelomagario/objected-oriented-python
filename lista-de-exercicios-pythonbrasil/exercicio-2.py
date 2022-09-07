21e5561c6770a231096a097923f087d6879bdd1a
Marcelo Magario <marcelomagario@gmail.com>
2022-09-07 19:22:56 +1000

    Solução do Exercício 2 - Lista Classes

--- lista-de-exercicios-pythonbrasil/exercicio-2.py
+++ lista-de-exercicios-pythonbrasil/exercicio-2.py
@@ -8,3 +8,26 @@ Ele deve pedir ao usuário que informe as medidades de um local.
 Depois, deve criar um objeto com as medidas e calcular a quantidade de pisos
 e de rodapés necessárias para o local.
 """
+
+class Square:
+    def __init__(self, side_len):
+        self.side_len = side_len
+
+    def changeSideLen(self, side_len):
+        self.side_len = side_len
+        print(f'This is the new side lenght: {self.side_len}')
+
+    def showSideLen(self):
+        print(self.side_len)
+
+    def calculateArea(self):
+        area = self.side_len ** 2
+        print(area)
+
+s1 = Square(15)
+s1.showSideLen()
+s1.changeSideLen(20)
+s1.showSideLen()
+s1.calculateArea()
+
+
