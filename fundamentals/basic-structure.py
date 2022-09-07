776e0491e39af3027d47146e37057c61cea1a996
Marcelo Magario <marcelomagario@gmail.com>
2022-09-07 11:31:37 +1000

    Create Methods -OOP

--- fundamentals/basic-structure.py
+++ fundamentals/basic-structure.py
@@ -1,13 +1,23 @@
 # Class
 # Syntax
-
 # Brand, Memory RAM, Video Board
 
 # Constructor. This method called when an object is created from the class
 #  it allow the class to initialize the attributes of a class.
-class Computer():
+class Computer:
     def __init__(self, brand, memory_ram, video_board):
-        self.video_board = video_board
-        self.memory_ram = memory_ram
         self.brand = brand
+        self.memory_ram = memory_ram
+        self.video_board = video_board
+
+    # Methods
+
+    def turn_on(self):
+        print(f"I'm turning the computer on the computer: {self.brand}")
+
+    def turn_off(self):
+        print(f"I'm turning the computer off the computer: {self.brand}")
+
+    def computer_settings(self):
+        print(self.brand, self.memory_ram, self.video_board)
 
