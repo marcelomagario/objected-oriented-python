b6bff6957a5b023f8ecea1ddad10dea6bcacd292
Marcelo Magario <marcelomagario@gmail.com>
2022-09-08 16:16:58 +1000

    Definição de atributos e métodos do Exercício 3 - Lista Classes

--- lista-de-exercicios-pythonbrasil/Exercicio-3.py
+++ lista-de-exercicios-pythonbrasil/Exercicio-3.py
@@ -9,9 +9,36 @@ class Rectangle:
         self.height = height
         self.width = width
 
-    def changeValue(self):
     def showValue(self):
+        print(f'my height is {self.height} and my width is {self.width}')
+
+    def changeValue(self, height, width):
+        self.height = height
+        self.width = width
+        print(f'my new height is {self.height} and my new width is {self.width}')
+
     def calculateArea(self):
+        area_rectangle = self.height * self.width
+        print(f'My area is {area_rectangle}')
+
     def calculatePerimeter(self):
+        perimeter_rectangle = (self.height * 2) + (self.width * 2)
+        print(f'My perimeter is {perimeter_rectangle}')
+
 
 # Implementation
+
+height = int(input('Digite a altura do retangulo: '))
+width = int(input('Digite a largura do retangulo: '))
+
+r1 = Rectangle(height, width)
+r1.showValue()
+r1.calculateArea()
+r1.calculatePerimeter()
+
+height = int(input('altere a altura do retangulo: '))
+width = int(input('altere a largura do retangulo: '))
+r1.changeValue(height, width)
+r1.showValue()
+r1.calculateArea()
+r1.calculatePerimeter()
