3e9b9c1e08a86e469b5d2e7ad636fcbfa1493878
Marcelo Magario <marcelomagario@gmail.com>
2022-09-08 15:09:30 +1000

    Solução do Exercício 3 - Lista Classes

---
+++ lista-de-exercicios-pythonbrasil/Exercicio-3.py
@@ -0,0 +1,17 @@
+"""
+Crie uma classe que modele um retangulo:
+
+"""
+
+class Rectangle:
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def changeValue(self):
+    def showValue(self):
+    def calculateArea(self):
+    def calculatePerimeter(self):
+
+# Implementation
