c7e9316f8a1faff1af6560b10ba48d12da3767f7
Marcelo Magario <marcelomagario@gmail.com>
2022-09-09 22:49:38 +1000

    Iplementação do método abastecerPorValor

--- lista-de-exercicios-pythonbrasil/exercicio-10-Bomba-de-combustivel.py
+++ lista-de-exercicios-pythonbrasil/exercicio-10-Bomba-de-combustivel.py
@@ -1,7 +1,22 @@
 # Bomba de Combustível e POO
 
 class BombaCombustivel:
-    def __init__(self, tipoCombustivel, valorLitro, quantidadeCombustivel):
-        self.tipoCombustivel = tipoCombustivel
-        self.valorLitro = valorLitro
-        self.quantidadeCombustivel = quantidadeCombustivel
+    def __init__(self, tipo_combustivel, valor_litro, quantidade_combustivel):
+        self.tipo_combustivel = tipo_combustivel
+        self.valor_litro = valor_litro
+        self.quantidade_combustivel = quantidade_combustivel
+
+    def abastecerPorValor(self):
+        reais_abastecidos = float(input('Informe quantos reais vc quer abastecer R$ __,__ : '))
+        qtde_litros = reais_abastecidos / self.valor_litro
+        print(f'Foi colocado {qtde_litros}L no seu veículo. ')
+
+    # def abastecerPorLitro(self):
+    # def alterarValor(self):
+    # def alterarCombustivel(self):
+    # def alterarQuantidadeCombustivel(self):
+
+ #implementação
+
+texaco = BombaCombustivel('Diesel', 2.2, 100)
+texaco.abastecerPorValor()
