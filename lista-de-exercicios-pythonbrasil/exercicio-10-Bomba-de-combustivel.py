bfa74cd8b607304379ef612644644698b76b53eb
Marcelo Magario <marcelomagario@gmail.com>
2022-09-09 15:48:25 +1000

    Definição de atributos do Exercício 10 - Lista Classes

---
+++ lista-de-exercicios-pythonbrasil/exercicio-10-Bomba-de-combustivel.py
@@ -0,0 +1,7 @@
+# Bomba de Combustível e POO
+
+class BombaCombustivel:
+    def __init__(self, tipoCombustivel, valorLitro, quantidadeCombustivel):
+        self.tipoCombustivel = tipoCombustivel
+        self.valorLitro = valorLitro
+        self.quantidadeCombustivel = quantidadeCombustivel
